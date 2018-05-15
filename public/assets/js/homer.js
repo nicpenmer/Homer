2bb64745f0d24e9b091865032b3b1917850f86b2
Dennyela Reever <dennyela.reever@gmail.com>
2018-05-14 20:14:54 -0400

    js

--- public/assets/js/homer.js
+++ public/assets/js/homer.js
@@ -1,17 +1,12 @@
 //***********Home Page Functionality****************
+
+//***********New user Functionality****************
 $(document).ready(function() {
 		//buton to display new profile form
 	$("#create").on("click", function() {
 		//alert
 		alert("Start your journey with Homer");
 		//render new profile form
-	});
-
-	//button to display login form
-	$("#login").on("click", function() {
-		//alert
-		alert("Welcome back, continue your journey");
-		//render login form
 	});
 
 	//button for creating new profile
@@ -45,6 +40,35 @@ $(document).ready(function() {
 
 		//render home profile page with stats for quiz
 	});
+
+//***********existing user Functionality****************
+	//button to display login form
+	$("#login").on("click", function() {
+		//alert
+		alert("Welcome back, continue your journey");
+		//render login form
+	});
+	//button for logging in
+	$("#submit").on("click", function() {
+		//grabs user input //check burger and train example...
+		var newUser = $(this).data("#newUser").val().trim();
+		var newPass  = $(this).data("#newPass").val().trim();
+		// creates local "temp" objest for new profile
+		var login = {
+			user: newUser,
+			password: newPass
+		};
+		//send PUT request.
+		$.ajax("/api/homer/" + login, {
+			type: "PUT",
+			data: login
+		}).then(
+			function() {
+				console.log("You are logged in", login);
+				//*******do we need anything else????
+			}
+		)
+		//** do we need to clear the create profile form (is that needed?)
 
 });
 
