d562b6a6034438a6701dd9b9a6a7016cdd89e704
Dennyela Reever <dennyela.reever@gmail.com>
2018-05-18 22:23:33 -0400

    login.js quiz.js

--- public/assets/js/quiz.js
+++ public/assets/js/quiz.js
@@ -1,18 +1,37 @@
 //***********Quiz Functionality****************
 $(document).ready(function() {
 
+	//build an array of the value for the quiz
+	var quiz = [
+		{
+			"name": "Dennyela",
+			"answers": [
+				1,
+				1,
+				1,
+			]
+		},
+	]
 
 	$("submitQuiz").on("click", function() {
-		// var for how many questions there are and home many answers the user filled out
-		var questions = 10;
-		var answers = 10;
-		
-		//checking to see if quiz is complete 
-		if (question.length === answers.length) {
-			//if so push values to database and store in user profile
-		} else {
-			console.log("please fill out all questions")
-		}
 
+		// var for how many questions there are and home many answers the user filled out
+		ajax("/api/quiz", + quiz, {
+			type: "POST",
+			data: quiz
+		}).then(
+			function(data) {
+			//checking to see if quiz is complete 
+				if (quiz.length === 10) {
+					//if so push values to database and store in user profile
+					res.json(true)
+				} else {
+					console.log("please fill out all questions")
+				}
+					}
+				)
 	});
+
+module.exports = quiz;
+
 });
